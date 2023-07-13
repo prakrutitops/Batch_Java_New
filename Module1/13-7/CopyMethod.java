bb367e1efba48625d29db13c9ddd19ff16c8abd2
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-07-13 12:26:16 +0530

    Add files via upload

---
+++ Module1/13-7/CopyMethod.java
@@ -0,0 +1,20 @@
+package com.a137;
+
+public class CopyMethod 
+{
+	public static void main(String[] args) 
+	{
+		
+		int A[] = {10,20,30,40,50};
+		
+		int B[]=new int[10];
+		
+		System.arraycopy(A, 1, B, 2, 3);
+		
+		for(int i=0;i<10;i++)
+		{
+			System.out.println(B[i]);
+		}
+		
+	}
+}
